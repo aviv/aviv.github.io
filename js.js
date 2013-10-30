0cfdf540996fd33795e681c70692e706674b7617
Aviv Nitsan <avnit7@gmail.com>
2013-10-29 23:38:40 -0700

    clickability fix, popup functionality

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
 function goto(u) {
   window.location = u
 }
+
+function showpopup(pid) {
+  document.getElementById(pid+'-plink').style.visibility='hidden'
+  popup = document.getElementById(pid+'-popup')
+  p = document.getElementById(pid)
+  popup.style.display = 'block'
+  popup.style.left = p.offsetLeft
+  popup.style.top = p.offsetTop
+  console.log(p.offsetLeft, p.offsetTop)
+}
