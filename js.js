fd135b4ee348332924e4e1e919cd1a271d7aab17
Aviv Nitsan <avnit7@gmail.com>
2013-11-10 15:20:27 -0800

    #4

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+window.openpopups = []
+
 function goto(u) {
   window.location = u
 }
@@ -5,9 +7,16 @@ function goto(u) {
 function showpopup(pid) {
   document.getElementById(pid+'-plink').style.visibility='hidden'
   popup = document.getElementById(pid+'-popup')
+  window.openpopups.push(popup)
   p = document.getElementById(pid)
   popup.style.display = 'block'
   popup.style.left = p.offsetLeft
   popup.style.top = p.offsetTop
-  console.log(p.offsetLeft, p.offsetTop)
+  // console.log(p.offsetLeft, p.offsetTop)
+}
+
+function hidepopups() {
+	for (var i=0; i<window.openpopups.length; i++) {
+		window.openpopups.pop().style.display = 'none'
+	}
 }
