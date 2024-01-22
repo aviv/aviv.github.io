c00b7403802e085269214422eaae3d69eb714772
Aviv Nitsan <2646916+aviv@users.noreply.github.com>
2024-01-22 08:49:02 -0800

    :scissors:

--- js/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  $(function() {
-    var address;
-    address = ['aviv', '@', 'berkeley', '.', 'edu'].join('');
-    return $('#email-dynamic').attr('href', "mailto:" + address);
-  });
-
-}).call(this);
